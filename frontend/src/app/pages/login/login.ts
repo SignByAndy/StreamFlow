67e80a9be7537cbfbaacdd147a462342f831e4d8
SignByAndy <andy.lopez04@icloud.com>
2026-02-12 04:30:21 +0100

    first commit for login

---
+++ frontend/src/app/pages/login/login.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.html'
+})
+export class LoginComponent {
+  email = '';
+  password = '';
+
+  onLogin() {
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
+}
